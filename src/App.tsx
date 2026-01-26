3480d2f2255ff631a50db470bbf16b28b2f72366
Isadora <isadoraloopes@hotmail.com>
2026-01-26 18:42:17 -0300

    Implementação de cabeçalho à página

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
 import Faq from "./pages/funcionalidades/Faq"
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from "./pages/home/Home";
+import Navbar from "./components/navbar/Navbar";
 
 
 export default function App() {
   return (
     <BrowserRouter>
     
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/faq" element={<Faq />} />
-      </Routes>
-
-     
+      <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/faq" element={<Faq />} />
+        </Routes>
     </BrowserRouter>
   )
 }
