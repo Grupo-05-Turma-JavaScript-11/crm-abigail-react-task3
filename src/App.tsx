96af0b6cf788963715409eaf0d67457f94b181f6
Isadora <isadoraloopes@hotmail.com>
2026-01-27 08:41:45 -0300

    Ajuste para merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
-import Faq from "./pages/funcionalidades/Faq"
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from "./pages/home/Home";
-import Navbar from "./components/navbar/Navbar";
+import { BrowserRouter, Routes } from 'react-router-dom';
 
 
 export default function App() {
   return (
     <BrowserRouter>
     
-      <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/faq" element={<Faq />} />
+
         </Routes>
     </BrowserRouter>
   )
