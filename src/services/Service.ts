54b96c8d0450438c0ce735c8ced5835e168d69a8
Isadora <isadoraloopes@hotmail.com>
2026-02-02 19:04:12 -0300

    Aplicando conexão com backend

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
+    baseURL: "https://crm-backend-e4sa.onrender.com"
 })
 
 export const cadastrarUsuario = async (url: string, dados: Object, setDados: Function) => {
